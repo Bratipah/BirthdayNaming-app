323fceb645a8103d2af32173d1c46eee1c161fe2
Bratipah <bratipahmh@gmail.com>
2020-08-22 22:59:43 +0300

    add Js &function

---
+++ script.js
@@ -0,0 +1,8 @@
+var male = parseInt(document.getElementById(male).value)
+var female = parseInt(document.getElementById(female).value)
+var bDate = parseInt(document.getElementById(bDate).value)
+var akanMaleName = ["Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame"]
+var akanFemaleName = ["Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama"]
+function() {
+
+}
