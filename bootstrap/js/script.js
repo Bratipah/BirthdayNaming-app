fcfe319e3cca97bfdf5502a2b6db4d3891cfb9ab
Bratipah <bratipahmh@gmail.com>
2020-08-23 03:17:15 +0300

    add about the app row

--- bootstrap/js/script.js
+++ bootstrap/js/script.js
@@ -6,17 +6,19 @@ var akanFemaleName = ["Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama"];
 function akanName() {
     var bdate = document.getElementById("bdate").value;
     var gender = document.getElementById("gender").value;
-    alert(gender)
+
     var CC = parseInt(bdate.substr(6, 7));
-    var DD = parseInt(bdate.substr(3, 4))
+    var DD = parseInt(bdate.substr(3, 4));
     var YY = parseInt(bdate.substr(8, 9));
     var MM = parseInt(bdate.substr(0, 1));
     var doW = parseInt((CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7);
 
-    if (gender === "Male") {
-        alert("You now posess the AkanName:" + akanMaleName[d0W])
+    if (month >= 12 || month <= 1) {
+        alert("Enter a month number between 1-12")
+    } else if (gender === "Male") {
+        alert("You now posess the AkanName:" + akanMaleName[doW])
     } else if (gender === "Female") {
-        alert("you now posess the AkanName:" + akanFemaleName[doW])
+        alert("You now posess the AkanName:" + akanFemaleName[doW])
     }
 
 
